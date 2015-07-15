9fb18af1f33f73832a2ab2585c829386c6e2c44e
Harry Grillo <harry@redbassett.com>
2015-07-15 04:31:51 -0400

    Implement Gulp for building SASS and Coffeescript
    Installing Gulp requires NPM (package.json file added). Gulp
    configuration is done in gulpfile.js. This commit also adds the
    node_modules and js directories to the .gitignore.
    
    Closes #4.

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+var coffee = require('gulp-coffee');
+var gutil = require('gulp-util');
+
+gulp.task('sass', function(){
+	return gulp.src('scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('css'));
+});
+
+gulp.task('coffee', function(){
+	return gulp.src('coffee/*.coffee')
+		.pipe(coffee()).on('error', gutil.log)
+		.pipe(gulp.dest('js'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('scss/*.scss', ['sass']);
+	gulp.watch('coffee/*.coffee', ['coffee']);
+});
+
+gulp.task('default', ['sass', 'coffee']);
